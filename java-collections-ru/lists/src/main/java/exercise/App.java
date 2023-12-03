fc5ab17d80c3e0a16017ba373f30577c249a5a1a
Andrey Borychev <borychev.au@gmail.com>
2023-12-03 18:58:10 +0300

    add App class and scrabble method

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,9 +1,37 @@
 package exercise;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
+public class App {
+    public static void main(String[] args) {
+        System.out.println(scrabble("thlxertwq", "hexlet"));
+    }
+    public static boolean scrabble(String chars, String word) {
+        boolean isPossible = true;
 
+        List<Character> charsAsList = new ArrayList<>();
+        List<Character> wordAsList = new ArrayList<>();
+
+        for (char ch : chars.toLowerCase().toCharArray()) {
+            charsAsList.add(ch);
+        }
+
+        for (char ch : word.toLowerCase().toCharArray()) {
+            wordAsList.add(ch);
+        }
+
+        for (Character symbolFromWord : wordAsList) {
+            if (!charsAsList.contains(symbolFromWord)) {
+                isPossible = false;
+            } else {
+                charsAsList.remove(symbolFromWord);
+            }
+        }
+
+        //mock
+        return isPossible;
+    }
+}
 //END
